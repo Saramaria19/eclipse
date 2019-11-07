03157d31da19c71cd30bc95f323c10f4f5674309
Saramaria19 <57485769+Saramaria19@users.noreply.github.com>
2019-11-07 09:14:42 -0300

    Adicionei arquivos via upload

---
+++ controleDeNotificacao/src/main/java/com/controleDeNotificacao/ControleDeNotificacaoApplication.java
@@ -0,0 +1,12 @@
+ package com.controleDeNotificacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeNotificacaoApplication { 
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeNotificacaoApplication.class, args);
+	}
+}
